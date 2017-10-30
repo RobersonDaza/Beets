b53ede1ae877ad62ee723d822a8b997b62e4e604
RobersonDaza <robersondaza@gmail.com>
2017-10-30 15:32:19 -0500

    create relationship controller, configurate relationship, create follower, modified design

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users do
+      member do
+          get :following, :followers
+      end
+  end
   resources :beets
+  resources :relationships
   get '/home' => 'pages#home'
   get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
